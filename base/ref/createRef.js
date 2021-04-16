e49389d2138d685ebe6c07301ccaadb6edc177a2
luoxiaobu <573567138@qq.com>
2021-04-16 17:53:51 +0800

    add lifecycle demo

--- base/ref/createRef.js
+++ base/ref/createRef.js
@@ -7,33 +7,31 @@ import ReactDOM from 'react-dom'
 
 //forwardRef requires a render function 
 // TextInput 使用 React.forwardRef 来获取传递给它的 ref，然后转发到它渲染的 DOM input 上
-let TextInput = React.forwardRef((props,ref)=>{
-    debugger
-    return (<div>{props.children}<input ref={ref}/></div>)
+let TextInput = React.forwardRef((props, ref) => {
+    return ( <div> { props.children } < input ref = { ref }/></div> )
 })
 
 console.log(TextInput)
-class Form extends React.Component{
-    constructor(props){
+class Form extends React.Component {
+    constructor(props) {
         super(props);
         this.firstInput = React.createRef();
     }
 
     componentDidMount() {
-       this.firstInput.current.focus()
+        this.firstInput.current.focus()
     }
 
-    render(){
-        return(
-            <div>
-               <TextInput ref={this.firstInput}>姓名</TextInput>
-               <TextInput>年龄</TextInput>
+    render() {
+        return (<div>
+            <TextInput ref = { this.firstInput } > 姓名 </TextInput> 
+            <TextInput> 年龄 </TextInput>
             </div>
         )
     }
 }
 
-let form = <Form></Form>
+let form = <Form> </Form>;
 //转换成一个object 对象
 console.log(form)
-ReactDOM.render(form,document.getElementById('root'))
+ReactDOM.render(form, document.getElementById('root'))
--- base/src/index.js
+++ base/src/index.js
@@ -1,40 +1,93 @@
-// 核心库 预渲染平台无关 将模板变成 一个对象 vdom {props,type,ref}
+// 核心库 预渲染平台无关 将模板变成一个对象 vodm {props,type,ref}
 import React from 'react';
 
-//渲染库 可以把react渲染到不同平台上 react-dom react-native react-canvas
+// 渲染库 可以把react 渲染到不同平台上 react-dom react-canvas react-native
 import ReactDom from 'react-dom';
-
 
 class Counter extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { number: 0, name: 'haha' };
-    }
-    // fiels class
-    handleClick = (event) => {
-        // 在当前批处理下 this.state.number 是不会改变的
-        // event.stopPropagation()
-        console.log(event)
-        this.setState({number:this.state.number + 1})
-        console.log(this.state.number)
-        this.setState({number:this.state.number + 1, name: 'xiaoyi'})
-        console.log(this.state.number)
-        this.setState((prestate)=>({number: prestate.number + 1}))
-        this.setState((prestate)=>({number: prestate.number + 1}))
-        this.setState((prestate)=>({number: prestate.number + 1}))
-        this.setState((prestate)=>({number: prestate.number + 1}))
-        this.setState({number:this.state.number + 1})
-        this.setState((prestate)=>({number: prestate.number + 1}))
-        setTimeout(()=>{console.log(this.state)})
+        this.state = {
+            number: 0,
+            name: 'xiaobu'
+        }
+        console.log('Counter 1.初始化状态状态对象 constructor');
     }
 
+    handleClick = () => {
+        this.setState({number: this.state.number + 1 })
+    }
+
+    componentWillMount() {
+        console.log('Counter 2.组件将要挂在 componentWillMount');
+    }
+    // 不常用生命周期
+    // 此方法仅作为性能优化的方式而存在
+    shouldComponentUpdate() {
+        console.log('Counter 5. 询问用户组件是否要更新, shouldComponentUpdate');
+        return true
+    }
+    componentWillUpdate() {
+        console.log('Counter 6. 组件将会更新，componentWillUpdate');
+    }
+    componentDidUpdate() {
+        console.log('Counter 7. 组件将会更新完成，componentDidUpdate');
+    }
+    componentDidMount() {
+        console.log('Counter 4.组件挂在成功 componentDidMount')
+    }
+    componentWillUnmount() {
+        // console.log('Counter')
+    }
     render() {
-        return(<div>
-            <h1 style={{color:'#ccc'}}>xiaobu test</h1>
-            <div>{this.state.number}</div>
-            <div><button onClick={this.handleClick}>点击一下</button></div>
-        </div>)
+        console.log('Counter 3.render 通过调用render方法获得虚拟DOM对象');
+        console.log(<ChildCounter {...this.state}/>)
+        return (
+            <div>
+                <p>{this.state.number}</p>
+                <button onClick={this.handleClick}>+</button>
+                <ChildCounter {...this.state}/>
+            </div>
+        )
     }
 }
 
-ReactDom.render(<Counter/>,document.getElementById('root'))
+class ChildCounter extends React.Component {
+    constructor(props){
+        super(props)
+        console.log(props)
+        this.state = { number: 0}
+        console.log('child: 1.constructor');
+    }
+    componentWillMount(){
+        console.log('child: 2.willmount')
+    }
+    componentDidMount(){
+        console.log('child: 4.didmount')
+    }
+    // 不常用生命周期 仅作为性能优化的方式存在
+    shouldComponentUpdate(){
+        console.log('child: 5.shouldUpdate')
+        return true
+    }
+    componentWillUpdate() {
+        console.log('child: 6.willUpdate')
+    }
+    componentDidUpdate(){
+        console.log('child: 7.didUpdate')
+    }
+    componentWillUnmount(){
+        console.log('child: willUnmount')
+    }
+    render(){
+        console.log('child: 3.render')
+        return (
+            <div>这是子计算器{this.state.number}</div>
+        )
+    }
+
+}
+
+
+
+ReactDom.render(<Counter/>, document.getElementById('root'))
