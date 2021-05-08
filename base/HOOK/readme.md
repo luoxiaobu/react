e415d2b78e714de97764779ebf48d3e17c4d1c15
luoxiaobu <573567138@qq.com>
2021-05-08 18:12:17 +0800

    add useState demo

---
+++ base/HOOK/readme.md
@@ -0,0 +1,46 @@
+关于hook 源码：
+
+// useState
+
+区分第一次执行，和非第一次执行
+第一次执行mount 阶段会初始化链表 
+
+hook->hook->hook : 只能在函数最外层调用 Hook。不要在循环、条件判断或者子函数中调用。 为了保持顺序
+
+
+queue.pending = update  ---> update---> update // 性能优化 批处理
+```js 
+const update = {
+  // 更新执行的函数
+  action,
+  // 与同一个Hook的其他更新形成链表，性能优化 批处理
+  next: null
+}
+
+```
+
+结构：
+
+hook{ queue.pending = update  ---> update---> update  }
+  |             ^                        |
+  |             |                        |
+  |             --------------------------
+  
+hook{ queue.pending = update  ---> update---> update  }
+                ^                        |
+                |                        |
+                --------------------------
+
+对于当前reactElement 对应一个 FiberNode
+
+```js 
+{
+//保存该FunctionComponent对应的Hooks链表
+this.memoizedState = null;
+// 对于 FunctionComponent，指函数本身，对于ClassComponent，指class，// 对于HostComponent，指DOM节点tagName
+this.type = null;
+// Fiber对应的真实DOM节点
+this.stateNode = null;
+}
+
+```
--- base/HOOK/useState.js
+++ base/HOOK/useState.js
@@ -12,13 +12,50 @@ import ReactDom from 'react-dom';
 // 怎么用：
 function Button() {
     let [number, setNumber] = useState(0)
-
+    let [haha, setHaha] = useState(0)
     return (<div>
         <div>{number}</div>
         <button onClick={() => setNumber(number + 1)}>+</button>
+        <button onClick={() => setHaha(haha + 1)}>+1</button>
     </div>)
 }
 
-ReactDom.render(<Button/>, document.getElementById('root'))
+ReactDom.render(<Button />, document.getElementById('root'))
 
-// 大概原理
+// 伪代码实现
+
+// let hookStates = [];//放着此组件的所有的hooks数据, 源码中是一个链表 且是存储在对应的 FiberNode 中
+// let hookIndex = 0; //代表当前的hooks的索引 
+// function useState(initialState) {
+//     let hookStates[hookIndex] = hookStates[hookIndex] || initialState;
+//     function setState(state) {
+//         let newState
+//         if (typeof state === 'function') {
+//             newState = state(hookStates[hookIndex]);
+//         } else {
+//             newState = state
+//         }
+//         if (Object.is(hookStates[hookIndex], newState)) {
+//             return;
+//         } else {
+//             hookStates[hookIndex] = newState;
+//             // 此处安排渲染，
+//             schedule();
+//         }
+//     }
+//     return [hookStates[hookIndex],setState];
+// }
+
+// 对于当前reactElement 对应一个 FiberNode
+
+// ```js 
+// {
+// //保存该FunctionComponent对应的Hooks链表
+// this.memoizedState = null;
+// // 对于 FunctionComponent，指函数本身，对于ClassComponent，指class，// 对于HostComponent，指DOM节点tagName
+// this.type = null;
+// // Fiber对应的真实DOM节点
+// this.stateNode = null;
+// }
+
+// ```
