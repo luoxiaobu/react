77b2a803ae47ab752b46625a87b1ce1b61a7fece
luoxiaobu <573567138@qq.com>
2021-05-28 00:48:52 +0800

    add  react-router-dom demo

---
+++ base/react-router-dom/HashRouter.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Router } from "react-router";
+
+import { createHashHistory as createHistory } from "history";
+class HashRouter extends React.Component {
+    history = createHistory(this.props);
+
+    render() {
+        return <Router history={this.history} children={this.props.children}></Router>
+    }
+}
+
+export default HashRouter;
---
+++ base/react-router-dom/index.js
@@ -0,0 +1,25 @@
+/**
+ * react-router-dom.
+ * BrowserRouter 
+ * HashRouter
+ * Link
+ * NavLink
+ */
+
+
+export {
+    Redirect,
+    Route,
+    Router,
+    Switch
+}
+from "react-router";
+
+export { default as BrowserRouter }
+from './BrowserRouter.js';
+export { default as HashRouter }
+from './HashRouter';
+export { default as Link }
+from './Link.js';
+export { default as NavLink }
+from './NavLink.js';
